b6dd4e7c35062f68474549ef070c4901b88ee26a
Damir Aliyev <damiraliyev@MacBook-Air-Damir.local>
2022-08-05 10:27:16 +0600

    refactor: create a generic onboarding view controller
    This commit adds a generic onboarding and we can reuse it multiple times.

--- Bankey/Bankey/OnboardingContainerViewController.swift
+++ Bankey/Bankey/OnboardingContainerViewController.swift
@@ -1,35 +1,26 @@
-//
-//  OnboardingContainerViewController.swift
-//  Bankey
-//
-//  Created by Damir Aliyev on 04.08.2022.
-//
-
 import UIKit
 
-class OnboardingContainerViewController: UIViewController{
-    
-    
+class OnboardingContainerViewController: UIViewController {
+
     let pageViewController: UIPageViewController
     var pages = [UIViewController]()
-    var currentVC: UIViewController{
-        didSet{
-            
+    var currentVC: UIViewController {
+        didSet {
         }
     }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
+        let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989.")
+        let page2 = OnboardingViewController(heroImageName: "world", titleText: "Move your money around the world quickly and securily.")
+        let page3 = OnboardingViewController(heroImageName: "thumbs", titleText: "Learn more at www.bankey.com.")
         
         pages.append(page1)
         pages.append(page2)
         pages.append(page3)
         
-        self.currentVC = pages.first!
+        currentVC = pages.first!
         
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -40,6 +31,7 @@ class OnboardingContainerViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .systemPurple
         
         addChild(pageViewController)
@@ -59,52 +51,56 @@ class OnboardingContainerViewController: UIViewController{
         pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
         currentVC = pages.first!
     }
-    
 }
 
-extension OnboardingContainerViewController: UIPageViewControllerDataSource{
+// MARK: - UIPageViewControllerDataSource
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         return getPreviousViewController(from: viewController)
     }
-    
+
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         return getNextViewController(from: viewController)
     }
+
     private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
         guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
         currentVC = pages[index - 1]
         return pages[index - 1]
     }
-    
+
     private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
         guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
         currentVC = pages[index + 1]
         return pages[index + 1]
     }
-    
+
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         return pages.count
     }
+
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return pages.firstIndex(of: self.currentVC) ?? 0
     }
 }
 
-class ViewController1: UIViewController{
+// MARK: - ViewControllers
+class ViewController1: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemRed
     }
 }
 
-class ViewController2: UIViewController{
+class ViewController2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
     }
 }
 
-class ViewController3: UIViewController{
+class ViewController3: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
---
+++ Bankey/Bankey/OnboardingViewController.swift
@@ -0,0 +1,66 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Damir Aliyev on 05.08.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController{
+    let heroImageName: String
+    let titleText: String
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+    init(heroImageName: String, titleText: String){
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+extension OnboardingViewController{
+    func style(){
+        view.backgroundColor = .systemBackground
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: heroImageName)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = titleText
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.numberOfLines = 0
+        
+    }
+    
+    func layout(){
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+}
