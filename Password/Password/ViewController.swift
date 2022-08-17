6ccdda269f7e991b0245e5c1a766ae63bb66ccf3
Damir Aliyev <damiraliyev@Air-Damir.lan>
2022-08-17 09:36:12 +0600

    create new password project

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Damir Aliyev on 17.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
